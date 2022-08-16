1d481c5dc1f4116042e25d17f8ce51567abd4dfa
kernie66 <kenneth.jagenheim@bredband.net>
2022-08-16 22:14:35 +0200

    Corrected Intl formats

--- src/helpers/getFlipValues.js
+++ src/helpers/getFlipValues.js
@@ -4,29 +4,35 @@ function capitalizeFirstLetter(string) {
 export default function getFlipValues() {
   const timeFormatOptions = {
     hour: '2-digit',
-    minute: '2-digit'
+    minute: '2-digit',
+    hour12: false,
   };
   const timeFormat = new Intl.DateTimeFormat(undefined, timeFormatOptions);
 
   const dateFormatOptions = {
     weekday: 'short',
-    day: '2-digit',
     month: 'short'
   };
   const dateFormat = new Intl.DateTimeFormat(undefined, dateFormatOptions);
 
+  const dayFormatOptions = {
+    day: '2-digit',
+  };
+  const dayFormat = new Intl.DateTimeFormat(undefined, dayFormatOptions);
+
   const now = new Date().getTime();
   const time = timeFormat.format(now).split(':');
   const date = dateFormat.format(now).split(' ');
+  const day = dayFormat.format(now);
   const dayOfWeek = new Date().getDay();
-
+  console.log(date[0], date[1], date[2])
   const value = {
     sep: ":",
     hours: time[0],
     minutes: time[1],
-    weekday: capitalizeFirstLetter(date[0]),
-    day: date[1],
-    month: capitalizeFirstLetter(date[2]),
+    weekday: capitalizeFirstLetter(date[1]),
+    day: day,
+    month: capitalizeFirstLetter(date[0].slice(0, 3)),
     dayOfWeek: dayOfWeek,
   };
 
