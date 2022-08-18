9ed6a887c2a4d001aecbc8d049996d74545f2a05
kernie66 <kenneth.jagenheim@bredband.net>
2022-08-19 00:17:51 +0200

    Better temperature display

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,10 +1,12 @@
+import { List } from "reactstrap";
+
 export default function Temperature() {
   return (
-    <span className="text-bottom outline-temp font-weight-bold">
-      <p>
+    <List type="unstyled" className="text-bottom outline-temp font-weight-bold border pe-4">
+      <li>
         <span>-24</span
         ><span style={{ fontSize: "75%" }}>.8&deg;C</span>
-      </p>
-    </span>
+      </li>
+    </List>
   );
 };
