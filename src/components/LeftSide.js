4e6a8e54ad089cfcf48ee8131362cd1d2af53fc8
kernie66 <kenneth.jagenheim@bredband.net>
2022-08-20 12:27:38 +0200

    Changed sunrise/sunset layout

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -3,13 +3,12 @@ import { List } from "reactstrap";
 export default function LeftSide() {
   return (
     <List type="unstyled" className="outline-md">
-      <li className="border-bottom outline-sm">Solen</li>
-      <li className="pb-2 pt-2">
+      <li className="border-bottom pb-2 pt-2">
         <img src="img/weather/sunrise-clipart-lg.png"
           width="56px" height="32px" alt="Sunrise" />
         <span className="align-bottom ps-2">05:30</span>
       </li>
-      <li>
+      <li className="pb-2 pt-2">
         <img src="img/weather/sunset-clipart-lg.png"
           width="56px" height="32px" alt="Sunset" />
         <span className="align-bottom ps-2">20:30</span>
