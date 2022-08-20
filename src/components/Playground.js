879acde8eb17167b5b6b5e5cbe599e3b106c56d7
kernie66 <kenneth.jagenheim@bredband.net>
2022-08-20 12:28:23 +0200

    Added playground component

---
+++ src/components/Playground.js
@@ -0,0 +1,22 @@
+import { Col, Container, Row } from "reactstrap";
+
+export default function Playground() {
+  const componentStyle = {
+    height: "60vh",
+  }
+  return (
+    <Container className="d-flex flex-column h-100 justify-content-between border" >
+      <div className="">
+        Row 1
+      </div>
+      <Row>
+        <Col>
+        Row 2
+        </Col>
+      </Row>
+      <div className="">
+        Row 3
+      </div>
+    </Container>
+  );
+};
