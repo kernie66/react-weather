9986cc6e031b9816101de142fc804e2acb1431f7
kernie66 <kenneth.jagenheim@bredband.net>
2022-09-04 00:43:56 +0200

    Corrected address, added current address

--- src/components/SelectLocation.js
+++ src/components/SelectLocation.js
@@ -1,19 +1,25 @@
 import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import { useAddress } from "../contexts/AddressProvider";
 import Map from "./Map";
 
 export default function SelectLocation({ modal, closeModal }) {
-
+  const { getAddress } = useAddress();
   const onOpened = () => {
 
   };
 
   return (
     <Modal fullscreen isOpen={modal} onOpened={onOpened} toggle={closeModal} fade={false}>
-      <ModalHeader toggle={closeModal}>
-        <h5>Ange adress för väder</h5>
+      <ModalHeader className="p-2 m-0" toggle={closeModal}>
+          <h5>
+            Ange adress för väder :&nbsp;&nbsp;
+            <span className="text-info">
+              {getAddress}
+            </span>
+          </h5>
       </ModalHeader>
       <ModalBody className="pt-0">
-        <Map />        
+        <Map />
       </ModalBody>
     </Modal>
   );
--- src/helpers/decodeAddress.js
+++ src/helpers/decodeAddress.js
@@ -1,5 +1,5 @@
 export default function decodeAddress(geocodeResult) {
-  const keys = Object.keys(geocodeResult.address_components);
+//  const keys = Object.keys(geocodeResult.address_components);
   const addressComponents = {
     street: '',
     sublocality: '',
@@ -9,14 +9,15 @@ export default function decodeAddress(geocodeResult) {
     natural_feature: '',
     park: '',
   };
-  console.log("Keys:", keys);
-  geocodeResult.address_components.forEach((key, index) => {
-    console.log(Object.keys(key), index);
-  });
-  const entries = Object.entries(geocodeResult.address_components);
-  console.log(entries);
+//  console.log("Keys:", keys);
+//  geocodeResult.address_components.forEach((key, index) => {
+//    console.log(Object.keys(key), index);
+//  });
+//  const entries = Object.entries(geocodeResult.address_components);
+//  console.log(entries);
+  let includeStreet = true;
   for (const [key, value] of Object.entries(geocodeResult.address_components)) {
-    console.log(`${key}: ${value.long_name}`);
+//    console.log(`${key}: ${value.long_name}`);
     value.types.forEach(val => console.log(val));
     if (value.types.includes('route')) {
       addressComponents.street = value.long_name + ', ';
@@ -27,27 +28,38 @@ export default function decodeAddress(geocodeResult) {
     if (value.types.includes('locality') || value.types.includes('postal_town')) {
       addressComponents.locality = value.long_name;
     };
-    if (value.types.includes('premise')) {
-      addressComponents.premise = value.long_name + ', ';
-    };
-    if (value.types.includes('point_of_interest')) {
-      addressComponents.point_of_interest = value.long_name + ', ';
-    };
-    if (value.types.includes('natural_feature')) {
-      addressComponents.natural_feature = value.long_name + ', ';
-    };
     if (value.types.includes('park')) {
       addressComponents.park = value.long_name + ', ';
+      includeStreet = false;
+    }
+    else {
+      if (value.types.includes('point_of_interest')) {
+        addressComponents.point_of_interest = value.long_name + ', ';
+        includeStreet = false;
+      }
+      else {
+        if (value.types.includes('premise')) {
+          addressComponents.premise = value.long_name + ', ';
+          includeStreet = false;
+        }
+        else {
+          if (value.types.includes('natural_feature')) {
+            addressComponents.natural_feature = value.long_name + ', ';
+            includeStreet = false;
+          };
+        };
+      };
     };
   };
-  const locationAddress = addressComponents.street
-                        + addressComponents.premise
-                        + addressComponents.point_of_interest
-                        + addressComponents.natural_feature
-                        + addressComponents.park 
-                        + addressComponents.sublocality
-                        + addressComponents.locality;
-  console.log(locationAddress);
+
+  const locationAddress = (includeStreet ? addressComponents.street : '')
+    + addressComponents.park
+    + addressComponents.point_of_interest
+    + addressComponents.premise
+    + addressComponents.natural_feature
+    + addressComponents.sublocality
+    + addressComponents.locality;
+  console.debug(locationAddress);
   return (
     locationAddress
   );
