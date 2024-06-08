67fb7df1e43bc86bce2f835ea0577f513af0f224
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-08 17:48:40 +0200

    Removed babel

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
-import WebFont from "webfontloader";
-import Background from "./components/Background";
-import Body from "./components/Body";
-import ErrorBoundary from "./components/ErrorBoundary";
-import Header from "./components/Header";
-import "./App.css";
-import AddressProvider from "./contexts/AddressProvider";
+import { useEffect } from 'react';
+import WebFont from 'webfontloader';
+import Background from './components/Background';
+import Body from './components/Body';
+import ErrorBoundary from './components/ErrorBoundary';
+import Header from './components/Header';
+import './App.css';
+import AddressProvider from './contexts/AddressProvider';
 
 export default function App() {
-
   // Load fonts used in the app
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ['Azeret Mono']
-      }
+        families: ['Azeret Mono'],
+      },
     });
   }, []);
 
@@ -30,4 +29,4 @@ export default function App() {
       </Background>
     </ErrorBoundary>
   );
-};
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootswatch/dist/superhero/bootstrap.min.css";
+import 'bootswatch/dist/superhero/bootstrap.min.css';
 // TODO: Note: Replace ^[theme]^ (examples: darkly, slate, cosmo, spacelab, and superhero. See https://bootswatch.com/ for current theme names.)
 //import 'bootstrap/dist/css/bootstrap.min.css';
+import '@mantine/core/styles.css';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 import 'react-bootstrap-typeahead/css/Typeahead.bs5.css';
-import "@pqina/flip/dist/flip.min.css";
+import '@pqina/flip/dist/flip.min.css';
 import './index.css';
 import './text.css';
 import './flip.css';
