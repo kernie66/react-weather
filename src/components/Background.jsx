0f66619f29d291badc3f2d7a755061840ecdb322
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-09 22:55:42 +0200

    Changed header to Mantine

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -11,7 +11,7 @@ export default function Background({ children }) {
     backgroundPosition: 'center',
     backgroundSize: 'center',
     backgroundRepeat: 'no-repeat',
-    //    width: '100vw',
+    // width: '100vw',
     height: '100vh',
   };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import { TbMap2 } from 'react-icons/tb';
 import { useState } from 'react';
-import { Col, Row } from 'reactstrap';
 import SelectLocation from './SelectLocation';
 import { useAddress } from '../contexts/AddressProvider';
 import FullScreenButton from './FullScreenButton.jsx';
+import { Button, Group, Text } from '@mantine/core';
 
 export default function Header() {
   const [modal, setModal] = useState(false);
@@ -20,21 +20,15 @@ export default function Header() {
   return (
     <>
       <SelectLocation modal={modal} closeModal={closeModal} />
-      <Row>
-        <Col xs="1">
-          <FullScreenButton />
-        </Col>
-        <Col xs="10" className="text-center outline-lg">
+      <Group justify="space-between">
+        <FullScreenButton />
+        <Text xs="10" className="text-center outline-lg">
           Väderstation :&nbsp;{getAddress.toString()}
-        </Col>
-        <Col
-          xs="1"
-          className="text-primary outline-lg"
-          onClick={selectAddress}
-        >
-          <TbMap2 />
-        </Col>
-      </Row>
+        </Text>
+        <Button variant="transparent" onClick={selectAddress}>
+          <TbMap2 size={36} color="darkred" />
+        </Button>
+      </Group>
     </>
   );
 }
