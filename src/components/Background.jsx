246df063eeda1366a9f8c8d5accf363ca1f9ec5a
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-10 23:53:55 +0200

    Background handling depends on aspect ratios

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from 'react';
 import { Container } from '@mantine/core';
+import { useViewportSize } from '@mantine/hooks';
+import { useImageSize } from 'react-image-size';
 
 export default function Background({ children }) {
   const [backgroundImage, setBackgroundImage] = useState('clear_day');
+  const [backgroundSize, setBackgroundSize] = useState('cover');
   const backgroundImageUrl =
-    'img/weather/' + backgroundImage + '.jpg';
+    '/img/weather/' + backgroundImage + '.jpg';
+  const { width, height } = useViewportSize();
+  const [dimensions] = useImageSize(backgroundImageUrl);
 
   const background = {
     backgroundImage: `url(${backgroundImageUrl})`,
     backgroundPosition: 'center',
-    backgroundSize: 'cover',
+    backgroundSize: backgroundSize,
     backgroundRepeat: 'no-repeat',
     // width: '100vw',
     height: '100vh',
   };
+
+  // Change image handling based on aspect ratios
+  useEffect(() => {
+    let newBgSize = 'cover';
+
+    if (dimensions) {
+      const imageAspect = dimensions.width / dimensions.height;
+      const screenAspect = width / height;
+      const ratio = imageAspect / screenAspect;
+
+      if (ratio > 1.1) {
+        newBgSize = 'auto 100%';
+      } else if (ratio < 0.8) {
+        newBgSize = '100% auto';
+      }
+    }
+    setBackgroundSize(newBgSize);
+  }, [dimensions, width, height, setBackgroundSize]);
 
   // Set up a timer
   useEffect(() => {
