7923cdc22fb7cd557a61d9895e9ff7820aa42a62
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-12 23:31:06 +0200

    Converted body to Mantine

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,55 +1,43 @@
-import { Col, Container, Row } from "reactstrap";
-import CurrentWeather from "./CurrentWeather";
-import ErrorBoundary from "./ErrorBoundary";
-import FlipDisplay from "./FlipDisplay";
-import Forecasts from "./Forecasts";
-import LeftSide from "./LeftSide";
-import RightSide from "./RightSide";
-import TemperatureDisplay from "./TemperatureDisplay";
+import CurrentWeather from './CurrentWeather';
+import ErrorBoundary from './ErrorBoundary';
+import FlipDisplay from './FlipDisplay';
+import Forecasts from './Forecasts';
+import LeftSide from './LeftSide';
+import RightSide from './RightSide';
+import TemperatureDisplay from './TemperatureDisplay';
+import { Container, Grid, Group, Stack } from '@mantine/core';
 
 export default function Body() {
   return (
-    <Container fluid className="d-flex flex-column p-0 m-0" style={{ height: "80vh" }}>
-      <Row className="justify-content-between w-100 m-0 mb-3" style={{ height: "55%" }}>
-        <Col xs="auto">
-          <ErrorBoundary>
-            <LeftSide />
-          </ErrorBoundary>
-        </Col>
-        <Col xs="auto" className="flex-grow-1">
-          <Container className="d-flex flex-column justify-content-around h-100 p-0">
-            <Row className="m-0">
-              <TemperatureDisplay />
-            </Row>
-            <Row>
-              <Container className="d-flex justify-content-evenly m-0">
-                <Col xs="auto">
-                  <ErrorBoundary>
-                    <FlipDisplay />
-                  </ErrorBoundary>
-                </Col>
-                <Col xs="auto">
-                  <ErrorBoundary>
-                    <CurrentWeather />
-                  </ErrorBoundary>
-                </Col>
-              </Container>
-            </Row>
-          </Container>
-        </Col>
-        <Col xs="auto">
-          <ErrorBoundary>
-            <RightSide />
-          </ErrorBoundary>
-        </Col>
-      </Row>
-      <Row>
-        <Container className="d-flex flex-wrap justify-content-between" style={{ width: "90%" }}>
+    <Container fluid>
+      <Group justify="space-between" h="55%">
+        <ErrorBoundary>
+          <LeftSide />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Stack>
+            <TemperatureDisplay />
+            <Group justify="space-between">
+              <ErrorBoundary>
+                <FlipDisplay />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <CurrentWeather />
+              </ErrorBoundary>
+            </Group>
+          </Stack>
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <RightSide />
+        </ErrorBoundary>
+      </Group>
+      <Group justify="center">
+        <Grid justify="space-around" w="90%">
           <ErrorBoundary>
             <Forecasts />
           </ErrorBoundary>
-        </Container>
-      </Row>
+        </Grid>
+      </Group>
     </Container>
   );
-};
+}
--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,26 +1,18 @@
-import { Col, Container, Row } from "reactstrap";
+import { Group, Image, Stack, Text } from '@mantine/core';
 
 export default function CurrentWeather() {
   return (
-    <Container className="d-flex justify-content-around m-0">
-      <Row>
-        <Col xs="auto" className="p-0 m-0">
-          <img src="weather_icons/PNG/256/day_partial_cloud.png" width="100px" height="100px" alt="Väder" />
-        </Col>
-        <Col xs="auto">
-          <Row>
-            <Col className="outline-lg text-center">
-              Växlande molnighet
-            </Col>
-          </Row>
-          <Row>
-            <Col className="outline-md text-center fs-2 pb-2">
-              Uppehåll
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-
-  )
+    <Group gap="md">
+      <Image
+        src="/weather_icons/PNG/256/day_partial_cloud.png"
+        width="100px"
+        height="100px"
+        alt="Väder"
+      />
+      <Stack align="center">
+        <Text className="outline-lg">Växlande molnighet</Text>
+        <Text className="outline-md">Uppehåll</Text>
+      </Stack>
+    </Group>
+  );
 }
