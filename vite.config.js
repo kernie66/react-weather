20a78a5822c719e4ae65174f0a705179acf737eb
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-17 21:31:22 +0200

    Added polyfill for legacy browsers

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react-swc';
+import legacy from '@vitejs/plugin-legacy';
 // import eslint from 'vite-plugin-eslint';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 
@@ -9,8 +11,21 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: 'build',
     },
-    plugins: [react(), eslintPlugin()],
+    plugins: [
+      react(),
+      eslintPlugin(),
+      legacy(/*{
+        modernTargets: [
+          'defaults',
+          'chrome >= 92',
+          'last 2 firefox version',
+          'last 2 safari version',
+          'ios >= 12',
+        ],
+      }*/),
+    ],
     server: {
+      host: '192.168.1.44',
       port: env.PORT,
       fs: {
         cachedChecks: false,
