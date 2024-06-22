50dab4d52133acc6e5a3ba32727ef9319c3e738c
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-06-22 20:31:22 +0200

    Changed temperature to Mantine

--- src/components/TemperatureDisplay.jsx
+++ src/components/TemperatureDisplay.jsx
@@ -1,21 +1,21 @@
 import Temperature from './Temperature';
 import MinMax from './MinMax';
-import { Col, Container } from 'reactstrap';
 import ErrorBoundary from './ErrorBoundary';
+import { Group } from '@mantine/core';
 
 export default function TemperatureDisplay() {
   return (
-    <Container className="d-flex justify-content-evenly m-0" style={{ color: "sandybrown" }}>
-      <Col xs="auto">
-        <ErrorBoundary>
-          <Temperature />
-        </ErrorBoundary>
-      </Col>
-      <Col xs="auto">
-        <ErrorBoundary>
-          <MinMax />
-        </ErrorBoundary>
-      </Col>
-    </Container>
+    <Group
+      justify="space-between"
+      mx="lg"
+      style={{ color: 'sandybrown' }}
+    >
+      <ErrorBoundary>
+        <Temperature />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <MinMax />
+      </ErrorBoundary>
+    </Group>
   );
-};
+}
