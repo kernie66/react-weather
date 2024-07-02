a138777332257768467af1a56467e0d1fabe891c
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-07-02 23:42:17 +0200

    Added current weather to left side info

--- src/components/LeftSide.jsx
+++ src/components/LeftSide.jsx
@@ -1,19 +1,26 @@
+import { useCurrentWeather } from '../utils/weatherQueries.js';
 import { Suntimes } from './Suntimes';
 import { Divider, Stack, Text } from '@mantine/core';
 
 export default function LeftSide() {
+  const { data: currentWeather } = useCurrentWeather();
+
   return (
     <Stack h="50vh" justify="space-around" gap="md">
       <Suntimes />
       <Stack gap={4}>
         <Text className="outline-sm">Luftfuktighet</Text>
         <Divider />
-        <Text className="outline-md">66%</Text>
+        <Text className="outline-md">
+          {currentWeather?.humidity}%
+        </Text>
       </Stack>
       <Stack gap={4}>
         <Text className="outline-sm">Vind</Text>
         <Divider />
-        <Text className="outline-md">12.0 m/s</Text>
+        <Text className="outline-md">
+          {currentWeather?.wind_speed.toFixed(1)} m/s
+        </Text>
         <Text className="outline-md">NNV</Text>
       </Stack>
     </Stack>
