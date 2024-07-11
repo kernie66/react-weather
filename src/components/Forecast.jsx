8fcf2c904cfb17caf16c9047fee9ec24900d04c4
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-07-11 14:56:55 +0200

    Better handling of forecast variables

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -7,11 +7,18 @@ import {
   Text,
 } from '@mantine/core';
 import { getWeatherIconUrl } from '../helpers/getImageUrl.js';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import dayjs from 'dayjs';
 import { capitalize } from 'radash';
+import { useSetState } from '@mantine/hooks';
 
 export default function Forecast({ hourlyWeather, moonPhase }) {
+  const [forecast, setForecast] = useSetState({
+    text: '',
+    temp: '',
+    rain: '',
+  });
+
   const weatherIcon = useMemo(
     () =>
       getWeatherIconUrl(
@@ -22,19 +29,30 @@ export default function Forecast({ hourlyWeather, moonPhase }) {
     [hourlyWeather, moonPhase]
   );
 
-  const forecastTime = dayjs.unix(hourlyWeather.dt);
-  let isToday = 'Idag';
-  if (!dayjs().isSame(dayjs.unix(hourlyWeather.dt), 'day')) {
-    isToday = capitalize(dayjs(forecastTime).format('ddd'));
-  }
-  const forecastText = isToday + dayjs(forecastTime).format(' HH:mm');
+  useEffect(() => {
+    const forecastTime = dayjs.unix(hourlyWeather.dt);
+    let isToday = 'Idag';
+    if (!dayjs().isSame(dayjs.unix(hourlyWeather.dt), 'day')) {
+      isToday = capitalize(dayjs(forecastTime).format('ddd'));
+    }
 
-  const forecastTemp = Math.round(hourlyWeather.temp);
-  let forecastRain = 'Uppehåll';
-  if (hourlyWeather.rain) {
-    forecastRain =
-      hourlyWeather.rain['1h'].toFixed(1).toString() + ' mm/h';
-  }
+    const forecastText =
+      isToday + dayjs(forecastTime).format(' HH:mm');
+
+    const forecastTemp = Math.round(hourlyWeather.temp);
+
+    let forecastRain = 'Uppehåll';
+    if (hourlyWeather.rain) {
+      forecastRain =
+        hourlyWeather.rain['1h'].toFixed(1).toString() + ' mm/h';
+    }
+
+    setForecast({
+      text: forecastText,
+      temp: forecastTemp,
+      rain: forecastRain,
+    });
+  }, [hourlyWeather, setForecast]);
 
   return (
     <Paper
@@ -45,7 +63,7 @@ export default function Forecast({ hourlyWeather, moonPhase }) {
       mih="12vh"
     >
       <Stack align="center" justify="space-around" gap={4} my={4}>
-        <Text className="outline-sm">{forecastText}</Text>
+        <Text className="outline-sm">{forecast.text}</Text>
         <Center>
           <Group gap="sm" justify="center">
             <Image
@@ -55,12 +73,12 @@ export default function Forecast({ hourlyWeather, moonPhase }) {
               alt="Halvklart"
             />
             <Text className="outline-temp-sm">
-              {forecastTemp}&deg;C
+              {forecast.temp}&deg;C
             </Text>
           </Group>
         </Center>
         <Text className="outline-sm" c="paleturquoise">
-          {forecastRain}
+          {forecast.rain}
         </Text>
       </Stack>
     </Paper>
