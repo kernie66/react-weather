dfd231c3e175acd7556361856c4f6689068f4f4e
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-07-21 16:54:47 +0200

    Now with history in address provider

--- src/contexts/AddressProvider.jsx
+++ src/contexts/AddressProvider.jsx
@@ -1,5 +1,11 @@
 import { useLocalStorage } from '@mantine/hooks';
-import { createContext, useContext, useMemo } from 'react';
+import { replaceOrAppend } from 'radash';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+} from 'react';
 
 const defaultLat = import.meta.env.VITE_DEFAULT_LATITUDE;
 const defaultLon = import.meta.env.VITE_DEFAULT_LONGITUDE;
@@ -12,53 +18,54 @@ export const defaultAddress = 'Rotebro, Sollentuna, Sverige';
 export const AddressContext = createContext();
 
 export default function AddressProvider({ children }) {
-  const [location, setLocation] = useLocalStorage({
+  const [myLocation, setMyLocation] = useLocalStorage({
     key: 'location',
     defaultValue: {
       address: defaultAddress,
       position: defaultPosition,
     },
   });
-  const [position, setPosition] = useLocalStorage({
-    key: 'position',
-    defaultValue: defaultPosition,
-  });
-  const [address, setAddress] = useLocalStorage({
-    key: 'address',
-    defaultValue: defaultAddress,
+  const [history, setHistory] = useLocalStorage({
+    key: 'locationHistory',
   });
 
-  console.log('Main location:', location);
+  console.log('Main location:', myLocation);
 
   const getAddress = useMemo(() => {
-    return location.address;
-  }, [location.address]);
+    return myLocation.address;
+  }, [myLocation.address]);
 
   const getPosition = useMemo(() => {
-    return location.position;
-  }, [location.position]);
+    return myLocation.position;
+  }, [myLocation.position]);
 
   const getLocation = useMemo(() => {
-    return location;
-  }, [location]);
+    return myLocation;
+  }, [myLocation]);
+
+  const setLocation = useCallback(
+    (newLocation) => {
+      console.log('Location updated:', newLocation);
+      setMyLocation(newLocation);
+      const newHistory = replaceOrAppend(
+        history,
+        newLocation,
+        (f) => f.address === newLocation.address
+      );
+      setHistory(newHistory);
+      console.log('History:', newHistory);
+    },
+    [setMyLocation, history, setHistory]
+  );
 
   const value = useMemo(() => {
     return {
-      setAddress,
       getAddress,
-      setPosition,
       getPosition,
       setLocation,
       getLocation,
     };
-  }, [
-    setAddress,
-    getAddress,
-    setPosition,
-    getPosition,
-    setLocation,
-    getLocation,
-  ]);
+  }, [getAddress, getPosition, setLocation, getLocation]);
 
   return (
     <AddressContext.Provider value={value}>
