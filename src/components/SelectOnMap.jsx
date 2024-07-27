0c4e9cd72827e0127fbdf0ba16fafb883622d67c
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-07-27 10:47:26 +0200

    Simplified marker, couldn't change popup to Mantine so removed it

--- src/components/SelectOnMap.jsx
+++ src/components/SelectOnMap.jsx
@@ -1,24 +1,11 @@
-import { InfoWindowF, MarkerF } from '@react-google-maps/api';
-import { useState } from 'react';
-import { Text } from '@mantine/core';
+import { MarkerF } from '@react-google-maps/api';
 import { useMapLocation } from '../contexts/MapLocationProvider.jsx';
 
 export default function SelectOnMap() {
   const { getMapLocation } = useMapLocation();
-  const [popover, setPopover] = useState(false);
 
   const clickOnMarker = (ev) => {
     console.log('Marker clicked');
-    setPopover(true);
-  };
-
-  const closeInfo = () => {
-    setPopover(false);
-    console.log('Info closed');
-  };
-
-  const infoOptions = {
-    pixelOffset: new window.google.maps.Size(0, -20),
   };
 
   return (
@@ -29,18 +16,6 @@ export default function SelectOnMap() {
         onClick={clickOnMarker}
         className="marked-position"
       />
-
-      {popover && (
-        <InfoWindowF
-          position={getMapLocation.position}
-          onCloseClick={closeInfo}
-          options={infoOptions}
-        >
-          <Text c="dodgerblue" fw={500} fz="h4">
-            {getMapLocation.address}
-          </Text>
-        </InfoWindowF>
-      )}
     </>
   );
 }
