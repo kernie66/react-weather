7dd6649a23ca5e0f919aeef3f0a2a05ac76fbbb3
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-08-03 11:12:46 +0200

    Updated suntimes with helper function

--- src/components/Suntimes.jsx
+++ src/components/Suntimes.jsx
@@ -1,46 +1,68 @@
 import { Divider, Group, Image, Stack, Text } from '@mantine/core';
 import dayjs from 'dayjs';
 import { getClipArtUrl } from '../helpers/getImageUrl.js';
-import SunCalc from 'suncalc';
 import { useWeatherPosition } from '../utils/weatherQueries.js';
+import { getSunTimes } from '../helpers/getSunInfo.js';
+import classes from '../css/Text.module.css';
 
 export default function SunTimes() {
   const { data: position } = useWeatherPosition();
 
-  const sunTimes = SunCalc.getTimes(
-    dayjs().toDate(),
-    position.lat,
-    position.lon
-  );
+  const { sunriseTime, sunsetTime, afterSunrise } = getSunTimes(
+    dayjs(),
+    position
+  ); // dayjs(sunTimes.sunrise).format('HH:mm');
 
-  const sunriseTime = dayjs(sunTimes.sunrise).format('HH:mm');
-  const sunsetTime = dayjs(sunTimes.sunset).format('HH:mm');
-
-  return (
-    <Stack gap={8}>
-      <Group gap={8} pb={4}>
+  const Sunrise = () => {
+    return (
+      <Group gap={8} justify="space-between" pb={4}>
         <Image
           src={getClipArtUrl('sunrise-clipart-lg.png')}
           w={56}
           h={32}
           alt="Sunrise"
         />
-        <Text pt={6} className="outline-md">
+        <Text
+          pt={6}
+          ta="right"
+          fw={500}
+          fz={24}
+          c="orange.4"
+          className={classes.outlineSingle}
+        >
           {sunriseTime}
         </Text>
       </Group>
-      <Divider />
-      <Group gap={8} my={0} pt={0}>
+    );
+  };
+
+  const Sunset = () => {
+    return (
+      <Group gap={8} justify="space-between" my={0} pt={0}>
         <Image
           src={getClipArtUrl('sunset-clipart-lg.png')}
           w={56}
           h={32}
           alt="Sunset"
         />
-        <Text pt={6} className="outline-md">
+        <Text
+          pt={6}
+          ta="right"
+          fw={500}
+          fz={24}
+          c="orange.4"
+          className={classes.outlineSingle}
+        >
           {sunsetTime}
         </Text>
       </Group>
+    );
+  };
+  return (
+    <Stack gap={8}>
+      {afterSunrise ? <Sunset /> : <Sunrise />}
+      <Divider />
+      {afterSunrise ? <Sunrise /> : <Sunset />}
     </Stack>
   );
 }
---
+++ src/helpers/getSunInfo.js
@@ -0,0 +1,52 @@
+import SunCalc from 'suncalc';
+import dayjs from 'dayjs';
+
+export const getSunTimes = (date, position) => {
+  let sunriseTime = '00:00';
+  let sunsetTime = '23:59';
+  let sunTimes = {};
+  let afterSunrise = false;
+
+  const currentTime = dayjs(date);
+  const nextDay = currentTime.add(1, 'day');
+
+  const currentSunTimes = SunCalc.getTimes(
+    dayjs(currentTime).toDate(),
+    position.lat,
+    position.lon
+  );
+
+  const nextSunTimes = SunCalc.getTimes(
+    dayjs(nextDay).toDate(),
+    position.lat,
+    position.lon
+  );
+  console.log('Sun times:', currentSunTimes, nextSunTimes);
+
+  if (currentTime.isBefore(dayjs(currentSunTimes.sunrise))) {
+    sunTimes.sunrise = dayjs(currentSunTimes.sunrise);
+  } else {
+    sunTimes.sunrise = dayjs(nextSunTimes.sunrise);
+    afterSunrise = true;
+  }
+
+  if (currentTime.isBefore(dayjs(currentSunTimes.sunset))) {
+    sunTimes.sunset = dayjs(currentSunTimes.sunset);
+  } else {
+    sunTimes.sunset = dayjs(nextSunTimes.sunset);
+    afterSunrise = false;
+  }
+
+  if (sunTimes.sunrise) {
+    sunriseTime = sunTimes.sunrise.format('HH:mm');
+  }
+  if (sunTimes.sunset) {
+    sunsetTime = sunTimes.sunset.format('HH:mm');
+  }
+
+  return {
+    sunriseTime,
+    sunsetTime,
+    afterSunrise,
+  };
+};
