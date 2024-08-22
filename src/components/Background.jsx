68f4422af2b1d86f7d38f756ddac0be29da77c32
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-08-22 23:26:19 +0200

    Removed useEffect from background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,45 +1,23 @@
-import { useEffect, useState } from 'react';
 import { Container } from '@mantine/core';
-import { useViewportSize } from '@mantine/hooks';
+import { useLogger, useViewportSize } from '@mantine/hooks';
 import { useImageSize } from 'react-image-size';
 import { getWeatherImageUrl } from '../helpers/getImageUrl.js';
-import { defaultBackgroundImage } from '../helpers/getWeatherTheme.js';
 import { useAtomValue } from 'jotai';
 import { backgroundImageState } from '../atoms/weatherThemeStates.js';
 
 export default function Background({ children }) {
   const backgroundImage = useAtomValue(backgroundImageState);
-  const [backgroundSize, setBackgroundSize] = useState('cover');
-  const [backgroundImageUrl, setBackgroundImageUrl] = useState(
-    getWeatherImageUrl(`${defaultBackgroundImage}.jpg`)
-  );
-  const { width, height } = useViewportSize();
-  const [dimensions] = useImageSize(backgroundImageUrl);
+  const screenSize = useViewportSize();
 
-  useEffect(() => {
-    if (backgroundImage) {
-      setBackgroundImageUrl(
-        getWeatherImageUrl(`${backgroundImage}.jpg`)
-      );
-    }
-  }, [backgroundImage]);
-
-  const background = {
-    backgroundImage: `url(${backgroundImageUrl})`,
-    backgroundPosition: 'center',
-    backgroundSize: backgroundSize,
-    backgroundRepeat: 'no-repeat',
-    // width: '100vw',
-    height: '100vh',
-  };
+  useLogger('Background', [{ backgroundImage }]);
 
   // Change image handling based on aspect ratios
-  useEffect(() => {
+  const getBackgroundSize = (imgSize, scrSize) => {
     let newBgSize = 'cover';
 
-    if (dimensions) {
-      const imageAspect = dimensions.width / dimensions.height;
-      const screenAspect = width / height;
+    if (imgSize) {
+      const imageAspect = imgSize.width / imgSize.height;
+      const screenAspect = scrSize.width / scrSize.height;
       const ratio = imageAspect / screenAspect;
 
       if (ratio > 1.1) {
@@ -48,11 +26,25 @@ export default function Background({ children }) {
         newBgSize = '100% auto';
       }
     }
-    setBackgroundSize(newBgSize);
-  }, [dimensions, width, height, setBackgroundSize]);
+    return newBgSize;
+  };
+
+  const backgroundImageUrl = getWeatherImageUrl(
+    `${backgroundImage}.jpg`
+  );
+  const [dimensions] = useImageSize(backgroundImageUrl);
+  const backgroundSize = getBackgroundSize(dimensions, screenSize);
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundPosition: 'center',
+    backgroundSize: backgroundSize,
+    backgroundRepeat: 'no-repeat',
+    // width: '100vw',
+    height: '100vh',
+  };
 
   return (
-    <Container fluid style={background}>
+    <Container fluid style={backgroundStyle}>
       {children}
     </Container>
   );
