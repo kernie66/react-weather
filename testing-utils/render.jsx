544df5a97c9de60dd2a768a772d2a421cf11e8d5
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-10-12 12:03:42 +0200

    Added query client and provider

--- testing-utils/render.jsx
+++ testing-utils/render.jsx
@@ -3,13 +3,22 @@ import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
 // Import your theme object
 import { createTheme } from '@mantine/core';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 const theme = createTheme({});
 
 export function render(ui) {
+  const queryClient = new QueryClient();
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }) => (
-      <MantineProvider theme={theme}>{children}</MantineProvider>
+      <MantineProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </MantineProvider>
     ),
   });
 }
