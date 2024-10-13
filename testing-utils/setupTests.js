24355db8230d3f7ba4bf93e14f069d1dd035516f
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-10-13 09:15:26 +0200

    Added dayjs configuration before tests

--- testing-utils/setupTests.js
+++ testing-utils/setupTests.js
@@ -1,7 +1,17 @@
 import { afterAll, beforeAll } from 'vitest';
 import { server } from '../src/mocks/server.js';
+import dayjs from 'dayjs';
+import 'dayjs/locale/sv';
+import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import calendar from 'dayjs/plugin/calendar.js';
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+  server.listen();
+
+  dayjs.locale('sv');
+  dayjs.extend(localizedFormat);
+  dayjs.extend(calendar);
+});
 
 afterEach(() => server.resetHandlers());
 
