36069d3a0ccf49d9c37abd7a4043d81ccf9671cc
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-10-23 22:51:51 +0200

    Testing UpdatedAt

--- src/components/__tests__/UpdatedAt.test.jsx
+++ src/components/__tests__/UpdatedAt.test.jsx
@@ -1,10 +1,24 @@
 import { render, screen } from '../../../testing-utils';
 import UpdatedAt from '../UpdatedAt.jsx';
+import weatherDataJson from '../../../bruno/weather_alert_response.json';
+import dayjs from 'dayjs';
+import { vi } from 'vitest';
 
-describe('UpdatedAt', () => {
-  it('should display the time from current weather', () => {
+const weatherDate = dayjs.unix(weatherDataJson.current.dt);
+const fakeTodayDate = new Date(weatherDate.toDate());
+
+describe('test UpdatedAt', () => {
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it('displays the time from current weather', async () => {
+    vi.setSystemTime(fakeTodayDate);
+
     render(<UpdatedAt />);
 
-    screen.debug();
+    expect(
+      await screen.findByText(/fredag 12 juli 2024 kl. 11:20/i)
+    ).toBeInTheDocument();
   });
 });
