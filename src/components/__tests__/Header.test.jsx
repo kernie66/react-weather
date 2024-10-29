cf53dc6fffd5dba6a1eba5358d75d241786828a5
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-10-29 23:07:48 +0100

    Full test coverage of Header and its history dialog

--- src/components/__tests__/Header.test.jsx
+++ src/components/__tests__/Header.test.jsx
@@ -55,7 +55,7 @@ describe('test Header and history selection', () => {
     expect(screen.getByText(defaultAddress)).toBeInTheDocument();
   });
 
-  it('renders the header with buttons and selects history location with mouse', async () => {
+  it('renders the header and selects history location with mouse', async () => {
     const user = userEvent.setup();
     localStorage.setItem(
       'locationHistory',
@@ -117,7 +117,7 @@ describe('test Header and history selection', () => {
     expect(screen.getByText(fakeLocationOption)).toBeInTheDocument();
   });
 
-  it('renders the header with buttons and selects history location with keyboard', async () => {
+  it('renders the header and selects history location with keyboard', async () => {
     const user = userEvent.setup();
     localStorage.setItem(
       'locationHistory',
@@ -128,7 +128,6 @@ describe('test Header and history selection', () => {
       f.address.toLowerCase().includes(searchTerm)
     );
 
-    console.log('Starting third test.....');
     render(<Header />);
 
     const historyButton = await screen.findByRole('button', {
@@ -155,7 +154,7 @@ describe('test Header and history selection', () => {
     await user.keyboard('bad');
     expect(historyInput1).toHaveValue('bad');
     const historyOptions = screen.getAllByRole('option');
-    expect(historyOptions).toHaveLength(2);
+    expect(historyOptions).toHaveLength(selected.length);
     expect(historyOptions[0]).toHaveTextContent(selected[0].address);
     expect(historyOptions[1]).toHaveTextContent(selected[1].address);
 
@@ -164,10 +163,35 @@ describe('test Header and history selection', () => {
     expect(
       await screen.findByText(selected[0].address)
     ).toBeInTheDocument();
+  });
 
-    // Click location name button again
+  it('renders the header and selects history location with clear from keyboard', async () => {
+    const user = userEvent.setup();
+    localStorage.setItem(
+      'locationHistory',
+      JSON.stringify(fakeLocationHistory)
+    );
+    const searchTerm = 'vik';
+    const [selected] = fork(fakeLocationHistory.toReversed(), (f) =>
+      f.address.toLowerCase().includes(searchTerm)
+    );
+
+    render(<Header />);
+
+    const historyButton = await screen.findByRole('button', {
+      name: /historik/i,
+    });
+    expect(historyButton).toBeInTheDocument();
+    expect(screen.getByText(defaultAddress)).toBeInTheDocument();
+    expect(screen.queryByRole('listbox')).toBeNull();
+
+    // Click location name button
     await user.click(historyButton);
     expect(await screen.findByRole('dialog')).toBeInTheDocument();
+    expect(screen.getByRole('listbox')).toBeInTheDocument();
+    expect(screen.getAllByRole('option')).toHaveLength(
+      fakeLocationHistory.length
+    );
 
     // Enter some search text in the history input
     const historyInput2 = screen.getByRole('textbox', {
@@ -177,11 +201,36 @@ describe('test Header and history selection', () => {
     expect(historyInput2).toHaveFocus();
     await user.keyboard('bad');
     expect(historyInput2).toHaveValue('bad');
-    const historyOptions2 = screen.getAllByRole('option');
-    expect(historyOptions2).toHaveLength(2);
-    expect(historyOptions2[0]).toHaveTextContent(selected[0].address);
-    expect(historyOptions2[1]).toHaveTextContent(selected[1].address);
+    expect(screen.getAllByRole('option')).toHaveLength(2);
 
-    screen.debug(undefined, Infinity);
+    // Clear the text with delete button
+    const deleteButton = screen.getByRole('button', {
+      name: /clear value/i,
+    });
+    expect(deleteButton).toBeVisible();
+    await user.click(deleteButton);
+    expect(historyInput2).toHaveValue('');
+    expect(screen.getAllByRole('option')).toHaveLength(
+      fakeLocationHistory.length
+    );
+
+    // Enter new search text
+    expect(historyInput2).not.toHaveFocus();
+    await user.click(historyInput2);
+    expect(historyInput2).toHaveFocus();
+    await user.keyboard(searchTerm);
+    expect(historyInput2).toHaveValue(searchTerm);
+
+    // Check the result of the search
+    const historyOptions2 = screen.getAllByRole('option');
+    expect(historyOptions2).toHaveLength(selected.length);
+    expect(historyOptions2[0]).toHaveTextContent(selected[0].address);
+    expect(historyOptions2[3]).toHaveTextContent(selected[3].address);
+
+    // Select the third option using arrow keys
+    await user.keyboard('{ArrowDown}{ArrowDown}{Enter}');
+    expect(
+      await screen.findByText(selected[2].address)
+    ).toBeInTheDocument();
   });
 });
