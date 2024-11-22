1f649d2245ea07586d7eb48d8e430c0f2a6a15e0
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2024-11-22 22:58:10 +0100

    Added network status

--- src/components/UpdatedAt.jsx
+++ src/components/UpdatedAt.jsx
@@ -2,15 +2,18 @@ import { Group, Text } from '@mantine/core';
 import classes from '../css/Text.module.css';
 import { useCurrentWeather } from '../hooks/weatherQueries.js';
 import * as dayjs from 'dayjs';
+import { useNetwork } from '@mantine/hooks';
 
 export default function UpdatedAt() {
   const { data: currentWeather } = useCurrentWeather();
+  const networkStatus = useNetwork();
 
   const updatedAt = dayjs.unix(currentWeather?.dt).format('LLLL');
+  const textColor = networkStatus.online ? 'yellow.3' : 'red.3';
 
   return (
     <Group justify="center" gap="xs" mt="sm">
-      <Text c="yellow.3" className={classes.outlineSingle}>
+      <Text c={textColor} className={classes.outlineSingle}>
         Uppdaterad:
       </Text>
       <Text span c="gray.0" className={classes.outlineSingle}>
