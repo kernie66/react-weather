98910ec5c126eaef16d3e837d664dadd74384c4d
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2025-01-03 20:37:06 +0100

    Completed UpdatedAt with online test

--- src/components/UpdatedAt.jsx
+++ src/components/UpdatedAt.jsx
@@ -1,4 +1,4 @@
-import { Group, Text } from '@mantine/core';
+import { Group, Loader, Text, VisuallyHidden } from '@mantine/core';
 import classes from '../css/Text.module.css';
 import { useCurrentWeather } from '../hooks/weatherQueries.js';
 import * as dayjs from 'dayjs';
@@ -9,10 +9,16 @@ export default function UpdatedAt() {
   const networkStatus = useNetwork();
 
   const updatedAt = dayjs.unix(currentWeather?.dt).format('LLLL');
-  const textColor = networkStatus.online ? 'yellow.3' : 'red.3';
+  const textColor = networkStatus.online ? 'green.3' : 'red.3';
 
   return (
     <Group justify="center" gap="xs" mt="sm">
+      {networkStatus.online ? null : (
+        <>
+          <Loader color={textColor} type="dots" h={16} />
+          <VisuallyHidden>Offline</VisuallyHidden>
+        </>
+      )}
       <Text c={textColor} className={classes.outlineSingle}>
         Uppdaterad:
       </Text>
--- src/components/__tests__/UpdatedAt.test.jsx
+++ src/components/__tests__/UpdatedAt.test.jsx
@@ -1,24 +1,46 @@
 import { render, screen } from '../../../testing-utils';
 import UpdatedAt from '../UpdatedAt.jsx';
-import weatherDataJson from '../../../bruno/weather_alert_response.json';
-import dayjs from 'dayjs';
 import { vi } from 'vitest';
+import { useNetwork } from '@mantine/hooks';
 
-const weatherDate = dayjs.unix(weatherDataJson.current.dt);
-const fakeTodayDate = new Date(weatherDate.toDate());
+vi.mock('@mantine/hooks', async (importOriginal) => {
+  const realHooks = await importOriginal();
+  return {
+    ...realHooks,
+    useNetwork: vi.fn(),
+  };
+});
 
 describe('test UpdatedAt', () => {
   afterAll(() => {
-    vi.useRealTimers();
+    vi.restoreAllMocks();
   });
 
-  it('displays the time from current weather', async () => {
-    vi.setSystemTime(fakeTodayDate);
+  it('displays the time from fake weather when online', async () => {
+    useNetwork.mockReturnValue({
+      online: true,
+    });
 
     render(<UpdatedAt />);
 
     expect(
       await screen.findByText(/fredag 12 juli 2024 kl. 11:20/i)
     ).toBeInTheDocument();
+    expect(
+      await screen.queryByText(/offline/i)
+    ).not.toBeInTheDocument();
+  });
+
+  it('displays the time from fake weather when offline', async () => {
+    useNetwork.mockReturnValue({
+      online: false,
+    });
+
+    render(<UpdatedAt />);
+
+    expect(
+      await screen.findByText(/fredag 12 juli 2024 kl. 11:20/i)
+    ).toBeInTheDocument();
+    expect(await screen.findByText(/offline/i)).toBeInTheDocument();
   });
 });
